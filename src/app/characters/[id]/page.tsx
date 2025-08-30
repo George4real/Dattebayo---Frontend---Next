45933ec0d4eddc1287ce72cde060193a1d17accd
George4real <jorgeenriquepg01@gmail.com>
2025-08-30 18:48:17 -0400

    Creacion de pagina de detalles

--- src/app/characters/[id]/page.tsx
+++ src/app/characters/[id]/page.tsx
@@ -1,8 +1,15 @@
+import CharacterDatails from "@/components/character/CharacterDatails"
+import { characterAPIResponseSchema } from "@/schemas/character"
+import { getCollectionById } from "@/server-action/getCollectionById"
 
-export default function page() {
+export default async function page(props: PageProps<'/characters/[id]'>) {
+
+    const {id} = await props.params
+    const characterAPIResponse = await getCollectionById('characters', id)
+    const character = characterAPIResponseSchema.parse(characterAPIResponse)
     return (
         <>
-                            
+            <CharacterDatails character={character}/>
         </>
     )
 }
