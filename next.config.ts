83886589e693fb4691d4328d1956b07e9b247540
George4real <jorgeenriquepg01@gmail.com>
2025-08-30 16:20:35 -0400

    Configuracion de deepseak para cargar mejor las imagenes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-
+  images: {
+    domains: ['static.wikia.nocookie.net'],
+    // Opcional: puedes usar remotePatterns para más control (Next.js 12.3+)
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'static.wikia.nocookie.net',
+        port: '',
+      },
+    ],
+  },
+  api: {
+    responseLimit: false,
+    externalResolver: true,
+    bodyParser: {
+      sizeLimit: '10mb',
+    },
+  }
+}
 export default nextConfig;
