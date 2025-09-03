fad49375a90fec82d989b42c50ac343c15c7786d
George4real <jorgeenriquepg01@gmail.com>
2025-09-02 22:59:55 -0400

    Extraer params y pasarlosa clientes hijos

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -1,14 +1,21 @@
 import CharacterList from "@/components/character/CharacterList";
-import {getCollections} from "@/server-action/getCollections-action";
-import { charactersAPIResponseSchema } from "@/schemas/character"
+import { getCollections } from "@/server-action/getCollections-action";
+import { charactersAPIResponseSchema } from "@/schemas/CharacterSchema"
+import PaginationComponent from "@/components/PaginationComponent";
 
-export default async function page() {
+export default async function page({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string }>
+}) {
 
-  const charactersAPIResponse = await getCollections('characters')
+  const page = (await searchParams).page
+  const charactersAPIResponse = await getCollections('characters', page)
   const characters = charactersAPIResponseSchema.parse(charactersAPIResponse)
   return (
     <>
-      <CharacterList characters={characters}/>
+      <CharacterList characters={characters} />
+      <PaginationComponent page={page}/>
     </>
   )
 }
