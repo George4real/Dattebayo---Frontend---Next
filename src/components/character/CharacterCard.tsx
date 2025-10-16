95acf00127b7e0389e1e9af09e3abaea9369fbd3
George4real <jorgeenriquepg01@gmail.com>
2025-10-16 16:35:20 -0400

    Actulizando componente img

--- src/components/character/CharacterCard.tsx
+++ src/components/character/CharacterCard.tsx
@@ -1,38 +1,49 @@
+"use client";
+
 import { Character } from "@/schemas/CharacterSchema";
 import {
-    Card,
-    CardAction,
-    CardContent,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card"
+  Card,
+  CardAction,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import Image from "next/image";
 import Link from "next/link";
 
+
 export default function CharacterCard({ character }: { character: Character }) {
-    return (
-        <>
-            <Card className="pt-0">
-                <div className="w-full h-64 relative">
-                    <Image
-                        src={character.images[0] || "/public/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.jpg"}
-                        alt={character.name} height={0} priority
-                        fill
-                        className="rounded-xl object-cover">
-                    </Image>
-                </div>
-                <CardHeader>
-                    <CardTitle className="text-center text-xl">{character.name}</CardTitle>
-                </CardHeader>
-                <CardContent className="flex justify-center">
-                    <CardAction >
-                        <Link href={`/characters/${character.id}`} className="uppercase border-2 rounded-2xl p-1 py-1" >See more</Link>
-                    </CardAction>
-                </CardContent>
-                <CardFooter>
-                </CardFooter>
-            </Card>
-        </>
-    )
+  return (
+    <>
+      <Card className="pt-0">
+        <div className="relative">
+          <Image
+            src={character.images[1]}
+            alt={character.name}
+            width={975}
+            height={730}
+            priority
+            className="rounded-xl object-cover"
+          />
+        </div>
+        <CardHeader>
+          <CardTitle className="text-center text-xl">
+            {character.name}
+          </CardTitle>
+        </CardHeader>
+        <CardContent className="flex justify-center">
+          <CardAction>
+            <Link
+              href={`/characters/${character.id}`}
+              className="uppercase border-2 rounded-2xl p-1 py-1"
+            >
+              See more
+            </Link>
+          </CardAction>
+        </CardContent>
+        <CardFooter></CardFooter>
+      </Card>
+    </>
+  );
 }
